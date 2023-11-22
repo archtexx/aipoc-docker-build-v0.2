1ccf322fe1945bf3503c0c2d0dc40fb28ad98e63
jjayarajdev <105743314+jjayarajdev@users.noreply.github.com>
2023-11-22 22:00:04 +0530

    Delete document_interaction directory

--- document_interaction/doc_Interaction_frontend/next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  env: {
-    OPENAI_API_KEY: "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",
-  },
-}
-
-module.exports = nextConfig
